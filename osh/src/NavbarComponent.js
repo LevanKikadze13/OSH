22f135087e20ce89f04da1aea7ec6e43d654d972
LevanKikadze13 <levanikokikadze@gmail.com>
2024-03-23 14:07:56 +0400

    changed it again

--- osh/src/NavbarComponent.js
+++ osh/src/NavbarComponent.js
@@ -1,37 +1,61 @@
 import React, { useState } from 'react';
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
-import './NavbarComponent'
-const NavbarComponent = () => {
-  const [expanded, setExpanded] = useState(false);
+import { Navbar, Nav, NavDropdown, Container, Offcanvas } from 'react-bootstrap';
 
-  const toggleMenu = () => {
-    setExpanded(!expanded);
-  };
+const AppNavbar = () => {
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleOffcanvasToggle = () => setShowOffcanvas(!showOffcanvas);
+  const handleOffcanvasClose = () => setShowOffcanvas(false);
 
   return (
-    <Navbar bg="light" expand="lg" expanded={expanded}>
-      <Navbar.Brand href="#">Logo</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleMenu} />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#">Option 1</Nav.Link>
-          <NavDropdown title="Option 2" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#">Dropdown Option 1</NavDropdown.Item>
-            <NavDropdown.Item href="#">Dropdown Option 2</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#">Separated Link</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="#">Option 3</Nav.Link>
-        </Nav>
-        <Nav>
-          <Button variant="outline-primary" className="mr-2">
-            Login
-          </Button>
-          <Button variant="primary">Register</Button>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar bg="light" expand="lg" className="mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">Logo</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" onClick={handleOffcanvasToggle} />
+          <Navbar.Offcanvas
+            show={showOffcanvas}
+            onHide={handleOffcanvasClose}
+            placement="end"
+            scroll={true}
+            backdrop={false}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Menu</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#">Option 1</Nav.Link>
+                <NavDropdown title="Option 2" id="offcanvas-dropdown">
+                  <NavDropdown.Item href="#">Dropdown Item 1</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Dropdown Item 2</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#">Dropdown Item 3</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#">Option 3</Nav.Link>
+                <Nav.Link href="#">Login</Nav.Link>
+                <Nav.Link href="#">Register</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#">Option 1</Nav.Link>
+              <NavDropdown title="Option 2" id="navbar-dropdown">
+                <NavDropdown.Item href="#">Dropdown Item 1</NavDropdown.Item>
+                <NavDropdown.Item href="#">Dropdown Item 2</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#">Dropdown Item 3</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#">Option 3</Nav.Link>
+              <Nav.Link href="#">Login</Nav.Link>
+              <Nav.Link href="#">Register</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
-export default NavbarComponent;
+export default AppNavbar;
