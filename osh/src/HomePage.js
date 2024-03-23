c3f5a12e2ba04b740badc7f720cd6b1a2151301b
LevanKikadze13 <levanikokikadze@gmail.com>
2024-03-23 22:49:27 +0400

    trying to add swiper

--- osh/src/HomePage.js
+++ osh/src/HomePage.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import NewsSlider from './NewsSlider';
 import './HomePage.css';
 import NavbarComponent from './NavbarComponent';
+import ResponsiveSwiper from './Slider';
 
 function HomePage() {
   const [displayBurgerMenu, setDisplayBurgerMenu] = useState(true)
@@ -14,52 +14,9 @@ function HomePage() {
 
   return (
     <div id="home-page">
-      {/* <div id="header">
-        <div className="header-content" id="header-content-left">
-          <h1 className="logo-container">
-            <img className="logo" src="https://oshe.ge/images/custom/logo-light.png" alt="Logo" />
-          </h1>
-        </div>
-        <div className='burger-menu'>
-          <div id="nav-content" className={displayBurgerMenu ? 'show' : 'hide'}>
-            <div className="header-content" id="header-content-middle">
-              <div id="list-container">
-                <ul className="navbar-nav" id="list">
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Option 1</a>
-                  </li>
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Option 2</a>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#">Action</a></li>
-                      <li><a className="dropdown-item" href="#">Another action</a></li>
-                      <li><hr className="dropdown-divider" /></li>
-                      <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Option 3</a>
-                  </li>
-                </ul>
-              </div>
+      <NavbarComponent/>
+      <ResponsiveSwiper></ResponsiveSwiper>
 
-            </div>
-            <div className="header-content header-content-right" id="header-content-right">
-              <a href="#" className="btn btn-primary me-2">Login</a>
-              <a href="#" className="btn ">Register</a>
-            </div>
-
-          </div>
-          <div className='burger-menu-container'>
-            <span class="material-symbols-outlined burger-menu-icon" onClick={handleBurgerMenuClick}>
-              menu
-            </span>
-          </div>
-        </div>
-
-      </div> */}
-      <NavbarComponent></NavbarComponent>
-      {/* <NewsSlider/> */}
     </div>
   );
 }
